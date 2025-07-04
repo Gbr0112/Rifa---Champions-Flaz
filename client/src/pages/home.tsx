904b47ec9ff3c3ef0aa7e29735c38a9a7f57ad00
Gbr0112 <152274282+Gbr0112@users.noreply.github.com>
2025-07-04 13:28:44 -0300

    home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -122,7 +122,7 @@ export default function Home() {
           <div className="text-center mb-4">
             <div className="inline-block p-4 bg-white rounded-lg">
               <img
-                src={`https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${pixCodeEncoded}`}
+                const pixCodeEncoded = encodeURIComponent(rawPixCode);
                 alt="QR Code PIX"
                 className="w-48 h-48 mx-auto"
               />
